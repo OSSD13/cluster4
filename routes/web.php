4f49fa3704ebff8ffee504428fce0bdac69af3d9
Pakkapon <Pakkapon2547@gmail.com>
2025-04-09 16:39:19 +0700

    fix conflict by pk kuy

--- routes/web.php
+++ routes/web.php
@@ -16,11 +16,11 @@ use App\Models\Branch;
 use PHPUnit\Runner\HookMethod;
 
 // @author : Pakkapon Chomchoey 66160080
-Route::get('/cluster4/login', function () {
+Route::get('/login', function () {
     return view('login');
 })->name('login');
 
-Route::get('/cluster4/logout', function () {
+Route::get('/logout', function () {
     Session::forget('google_user');
     Session::flush();
     Auth::logout();
@@ -35,46 +35,44 @@ Route::get('/cluster4/auth/google/callback', [GoogleLoginController::class, 'goo
 Route::get('/', [UserController::class, 'index']);
 
 //kuy mork
-Route::get('/cluster4/dashboard', [DashboardController::class, 'branchGrowthRate'])->name('dashboard.branch.growth');
+Route::get('/dashboard', [DashboardController::class, 'branchGrowthRate'])->name('dashboard.branch.growth');
 
-Route::get('/cluster4/home', [HomeController::class, 'index'])->name('home');
+Route::get('/home', [HomeController::class, 'index'])->name('home');
 
-Route::get('/cluster4/manage-user', [UserController::class, 'index'])->name('manage.user');
+Route::get('/manage-user', [UserController::class, 'index'])->name('manage.user');
 
-Route::get('/cluster4/add-user', [UserController::class, 'add_user'])->name('add.user');
+Route::get('/add-user', [UserController::class, 'add_user'])->name('add.user');
 
-Route::post('/cluster4/add-user', [UserController::class, 'create'])->name('create.user');
+Route::post('/add-user', [UserController::class, 'create'])->name('create.user');
 
-Route::delete('/cluster4/delete-user', [UserController::class, 'delete_user'])->name('delete.user');
+Route::delete('/delete-user', [UserController::class, 'delete_user'])->name('delete.user');
 
-Route::get('/cluster4/edit-user/{id}', [UserController::class, 'edit_user']);
+Route::get('/edit-user/{id}', [UserController::class, 'edit_user']);
 
-Route::put('/cluster4/edit-user', [UserController::class, 'edit_action'])->name('edit.user');
+Route::put('/edit-user', [UserController::class, 'edit_action'])->name('edit.user');
 
 // Route::get('/cluster4/branchMyMap', [branchController::class, 'index'])->name('branchMyMap');
 
-Route::get('/cluster4/map', MapLocation::class)->name('map');
+Route::get('/map', MapLocation::class)->name('map');
 
 
-Route::get('/cluster4/order-detail/{br_id}', [OrderController::class, 'order_detail'])->name('orderDetail');
+Route::get('/order-detail/{br_id}', [OrderController::class, 'order_detail'])->name('orderDetail');
 // Route::get('/cluster4/branch-detail/{br_id}', [BranchController::class, 'branch_detail'])->name('branchDetail');
 // Route::get('/cluster4/edit-branch-detail/{br_id},{br_mount}', [BranchController::class, 'branch_detail'])->name('branchDetail');
 
 
-Route::get('/cluster4/order', [OrderController::class, 'index'])->name('order');
+Route::get('/order', [OrderController::class, 'index'])->name('order');
 
-Route::get('/cluster4/add-order', [OrderController::class, 'add_order']);
+Route::get('/add-order', [OrderController::class, 'add_order']);
 
 //@author Thakdanai
 // หน้าแก้ไขคำสั่งซื้อ
-Route::get('/cluster4/editOrder/{od_id}', [OrderController::class, 'editOrder'])->name('edit.order');
+Route::get('/editOrder/{od_id}', [OrderController::class, 'editOrder'])->name('edit.order');
 // อัปเดตคำสั่งซื้อ
-Route::put('/cluster4/edit-order/{od_id}', [OrderController::class, 'update'])->name('update.order');
-Route::get('/cluster4/order-detail/{br_id}', [OrderController::class, 'order_detail'])->name('order.detail');
+Route::put('/edit-order/{od_id}', [OrderController::class, 'update'])->name('update.order');
+Route::get('/order-detail/{br_id}', [OrderController::class, 'order_detail'])->name('order.detail');
 
 // ลบคำสั่งซื้อ (ใช้วิธีเปลี่ยนยอดขายเป็น 0)
-Route::post('/cluster4/delete-order/{id}', [OrderController::class, 'delete_order_detail'])->name('delete.order');
+Route::post('/delete-order/{id}', [OrderController::class, 'delete_order_detail'])->name('delete.order');
 
-Route::get('/cluster4/order-status', [OrderController::class, 'status'])->name('order.status');
-
-Route::get('/cluster4/employee', [UserController::class, 'Emp_GrowRate']);
+Route::get('/order-status', [OrderController::class, 'status'])->name('order.status');
